e889031a4e07d798203cde3c90c1f00b4ae755db
ZaL <z4ls@live.com>
2016-02-17 02:07:53 +0300

    fix font

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,7 @@
         <div class="navbar-header">
             <div class="container">
                 <a class="navbar-brand" href="{{ Auth::check() ? action('HomeController@home') : '#' }}"><img src="/images/logo.png" class="logo" alt=""></a>
-                <a class="btn responsive-menu pull-right" data-toggle="collapse" data-target=".navbar-collapse"><i
-                            class='fa fa-bars'></i></a>
+                <a class="btn responsive-menu pull-right" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
 
             </div>
             <!-- /.container -->
