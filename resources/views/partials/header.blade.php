7f1bbb30e7136e4d022f77994c10722a8cf5e8a8
ZaL <z4ls@live.com>
2016-02-17 02:01:28 +0300

    minify

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ Auth::check() ? action('HomeController@home') : '#' }}"><img src="/images/logo.png" class="logo" alt=""></a>
                 <a class="btn responsive-menu pull-right" data-toggle="collapse" data-target=".navbar-collapse"><i
-                            class='icon-menu-1'></i></a>
+                            class='fa fa-bars'></i></a>
 
             </div>
             <!-- /.container -->
