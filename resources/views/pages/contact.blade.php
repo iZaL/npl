e31f174cd893a5108fea642b2de161fea29c55ba
ZaL <z4ls@live.com>
2016-02-24 14:15:57 +0300

    add contact info

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,69 +6,76 @@
 @endsection
 
 @section('middle')
-<section id="contact-form">
-    <div class="container ">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="row">
+    <section id="contact-form">
+        <div class="container ">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="row">
 
-                    <div class="col-sm-6 inner-right-sm">
+                        <div class="col-sm-6 inner-right-sm">
 
-                        <h2>Leave a Message</h2>
+                            <h2>Leave a Message</h2>
 
-                        <form id="contactform" class="forms" action="/contact" method="post">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <input type="text" name="name" class="form-control" placeholder="Name (Required)">
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
+                            <form id="contactform" class="forms" action="/contact" method="post">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <input type="text" name="name" class="form-control" placeholder="Name (Required)">
+                                    </div><!-- /.col -->
+                                </div><!-- /.row -->
 
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <input type="email" name="email" class="form-control" placeholder="Email (Required)">
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <input type="email" name="email" class="form-control" placeholder="Email (Required)">
+                                    </div><!-- /.col -->
+                                </div><!-- /.row -->
 
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <textarea name="body" class="form-control" placeholder="Enter your message ..."></textarea>
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <textarea name="body" class="form-control" placeholder="Enter your message ..."></textarea>
+                                    </div><!-- /.col -->
+                                </div><!-- /.row -->
 
-                            <button type="submit" class="btn btn-default btn-submit">Submit message</button>
+                                <button type="submit" class="btn btn-green">Send Message</button>
 
-                        </form>
+                            </form>
 
-                        <div id="response"></div>
+                            <div id="response"></div>
 
-                    </div><!-- ./col -->
+                        </div><!-- ./col -->
 
-                    <div class="col-sm-6 inner-left-sm border-left">
+                        <div class="col-sm-6 inner-left-sm border-left">
 
-                        <h2>Contacts</h2>
-                        <p> We love to hear from you :) </p>
-                        <h3>No Problem Learning</h3>
-                        <ul class="contacts">
-                            <li><i class="icon-location contact"></i> 84 Street, City, State 24813</li>
-                            <li><i class="icon-mobile contact"></i> +00 (123) 456 78 90</li>
-                            <li><a href="mailto:info@reen.com"><i class="icon-mail-1 contact"></i> info@no-problem-learning.com</a></li>
-                        </ul><!-- /.contacts -->
+                            <h2>Contacts</h2>
+                            <p> We love to hear from you :) </p>
+                            <h2>No Problem Learning</h2>
+                            <h3>Managed by Ideas Owners Co</h3>
+                            <ul class="contacts">
+                                <li>
+                                    <p>
+                                        <i class="fa fa-map-marker"></i>
+                                        Sharq, Ahmad AL-Jaber St.
+                                        AL-Tejari Bank Building – 6th floor - Opposite to Dar AL-Awadhi Tower
+                                    </p>
+                                </li>
+                                <li><i class="fa fa-phone"></i>+965</li>
+                                <li><i class="fa fa-envelope"></i><a href="info@ideasowners.com"> info@ideasowners.com</a></li>
+                            </ul><!-- /.contacts -->
 
-                        <div class="social-network">
-                            <h3>Social</h3>
-                            <ul class="social">
-                                <li><a href="#"><i class="icon-s-facebook"></i></a></li>
-                                <li><a href="#"><i class="icon-s-gplus"></i></a></li>
-                                <li><a href="#"><i class="icon-s-twitter"></i></a></li>
-                            </ul><!-- /.social -->
-                        </div><!-- /.social-network -->
+                            <div class="social-network">
+                                <h3>Social</h3>
+                                <h2>
+                                    <a href="https://ideasowners.net/" target="_blank"><i class="fa fa-globe"></i></a>
+                                    <a href="https://twitter.com/ideasowners" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://instagram.com/ideasowners" target="_blank"><i class="fa fa-instagram"></i></a>
+                                </h2>
+                            </div><!-- /.social-network -->
 
-                    </div><!-- /.col -->
+                        </div><!-- /.col -->
 
-                </div><!-- /.row -->
-            </div><!-- /.col -->
-        </div><!-- /.row -->
+                    </div><!-- /.row -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
 
-    </div><!-- /.container -->
-</section>
+        </div><!-- /.container -->
+    </section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,38 +1,26 @@
 <footer id="footer" style="background-color: #333A40;">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-sm-6 inner">
-                <h4>Who we are</h4>
-                <p></p>
-            </div><!-- /.col -->
-
-            <div class="col-md-4 col-sm-6 inner">
+            <div class="col-md-12 col-sm-12 inner">
                 <h4>Get In Touch</h4>
-                <p></p>
                 <ul class="contacts">
-                    <li><i class="icon-location contact"></i> 84 Street, City, State 24813</li>
-                    <li><i class="icon-mobile contact"></i> +00 (123) 456 78 90</li>
-                    <li><a href="#"><i class="icon-mail-1 contact"></i> info@no-problem-learning.com</a></li>
+                    <li>
+                        <p>
+                            <i class="fa fa-map-marker"></i>&nbsp;
+                            Sharq, Ahmad AL-Jaber St. <br>
+                            AL-Tejari Bank Building – 6th floor - Opposite to Dar AL-Awadhi Tower
+                        </p>
+                    </li>
+                    <li><i class="fa fa-phone"></i>&nbsp;+965</li>
                 </ul><!-- /.contacts -->
             </div><!-- /.col -->
-
-            <div class="col-md-4 col-sm-6 inner">
-                <h4>Subscribe to our Newsletter</h4>
-                <p>To get latest updates and news, Subscribe to our Newsletter</p>
-                <form id="newsletter" class="form-inline newsletter" role="form">
-                    <label class="sr-only" for="exampleInputEmail">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail" placeholder="Enter your email address">
-                    <button type="submit" class="btn btn-default btn-submit">Subscribe</button>
-                </form>
-            </div><!-- /.col -->
-
         </div>
 
-        {{--<div class="col-sm-12 inner">--}}
-        {{--<div class="copyright-text text-center ">--}}
-        {{--<p>© 2015 <a href="http://no-problem-learning.com">No Problem Learning</a>. All rights--}}
-        {{--reserved. Developed by <a target="_blank" href="http://www.ideasowners.net">IdeasOwners</a></p>--}}
-        {{--</div>--}}
-        {{--</div>--}}
+        <div class="col-sm-12 inner">
+            <div class="copyright-text text-center ">
+                <p>© 2015 <a href="http://no-problem-learning.com">No Problem Learning</a>.&nbsp; All rights
+                    reserved. Developed and managed by <a target="_blank" href="http://www.ideasowners.net">IdeasOwners</a> Kuwait</p>
+            </div>
+        </div>
     </div>
 </footer>
