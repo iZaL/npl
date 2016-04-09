f89298a207a96d90703fdc59b80d37bbc618049a
ZaL <z4ls@live.com>
2016-04-09 18:47:08 +0300

    fix count query

--- resources/views/modules/blog/index.blade.php
+++ resources/views/modules/blog/index.blade.php
@@ -12,31 +12,34 @@
                 <h2><a href="{{ action('BlogController@show',$post->id) }}">{{ $post->title }}</a></h2>
 
                 <div class="row">
-                    <div class="col-md-3">
-                        <a href="{{ action('BlogController@show',$post->id) }}">
-                            @if($post->thumbnail)
-                                <img src="/uploads/thumbnail/{{ $post->thumbnail->name}}"
-                                     class="img-responsive img-thumbnail">
+                    @if($post->thumbnail)
+                        <div class="col-md-3">
+                            <a href="{{ action('BlogController@show',$post->id) }}">
+                                @if($post->thumbnail)
+                                    <img src="/uploads/thumbnail/{{ $post->thumbnail->name}}"
+                                         class="img-responsive img-thumbnail">
+                                @else
+                                    <img src="http://placehold.it/150x100/EEEEEE" class="img-responsive img-thumbnail">
+                                @endif
+                            </a>
+                        </div>
+                        <div class="col-md-9 blog-post">
                             @else
-                                <img src="http://placehold.it/150x100/EEEEEE" class="img-responsive img-thumbnail">
-                            @endif
-                        </a>
-                    </div>
-                    <div class="col-md-9 blog-post">
-                        {!! str_limit($post->description,300) !!}
-                    </div>
+                                <div class="col-md-12 blog-post">
+                                    @endif
+                                    {!! str_limit($post->description,300) !!}
+                                </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 ">
+                                <button class="btn btn-default mTop10"><a
+                                            href="{{ action('BlogController@show',$post->id) }}">More</a></button>
+                            </div>
+                        </div>
+
+                        <hr>
+                        @endforeach
+
                 </div>
-                <div class="row">
-                    <div class="col-md-12 ">
-                        <button class="btn btn-default mTop10"><a
-                                    href="{{ action('BlogController@show',$post->id) }}">More</a></button>
-                    </div>
-                </div>
-
-                <hr>
-            @endforeach
-
-
         </div>
-    </div>
 @endsection
